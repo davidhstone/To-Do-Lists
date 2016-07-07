91e7fb24fdd1e8c5014c1111a779748361e94423
davidhstone <d_dave27@hotmail.com>
2016-07-07 12:38:20 -0400

    added custom layouts

--- To-Do-Lists/app/src/main/java/com/example/davidstone/to_do_lists/MainActivity.java
+++ To-Do-Lists/app/src/main/java/com/example/davidstone/to_do_lists/MainActivity.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,13 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitle("Title");
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.listsrecyclerview);
+
+        LinearLayoutManager linearLayoutManager =
+                new LinearLayoutManager(this,LinearLayoutManager.VERTICAL, false);
+
+        mRecyclerView.setLayoutManager(linearLayoutManager);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
--- To-Do-Lists/app/src/main/java/com/example/davidstone/to_do_lists/ToDoListActivity.java
+++ To-Do-Lists/app/src/main/java/com/example/davidstone/to_do_lists/ToDoListActivity.java
@@ -4,10 +4,14 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class ToDoListActivity extends AppCompatActivity {
+
+    RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +19,14 @@ public class ToDoListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_to_do_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        toolbar.setTitle("Second Title");
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.todolistrecyclerview);
+
+        LinearLayoutManager linearLayoutManager =
+                new LinearLayoutManager(this,LinearLayoutManager.VERTICAL, false);
+
+        mRecyclerView.setLayoutManager(linearLayoutManager);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
