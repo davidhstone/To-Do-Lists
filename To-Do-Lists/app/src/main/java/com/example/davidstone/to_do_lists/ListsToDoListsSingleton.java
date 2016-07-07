38ddb7bd87f9aaca3e2f89c5fa8d3acd6da6c40b
davidhstone <d_dave27@hotmail.com>
2016-07-07 18:01:29 -0400

    Same as last commit but with first try Singleton... singleton not yet in main though

--- To-Do-Lists/app/src/main/java/com/example/davidstone/to_do_lists/ListsToDoListsSingleton.java
+++ To-Do-Lists/app/src/main/java/com/example/davidstone/to_do_lists/ListsToDoListsSingleton.java
@@ -1,15 +1,54 @@
 package com.example.davidstone.to_do_lists;
 
+import java.util.LinkedList;
+
 /**
  * Created by davidstone on 7/7/16.
  */
 public class ListsToDoListsSingleton {
-    private static ListsToDoListsSingleton ourInstance = new ListsToDoListsSingleton();
 
-    public static ListsToDoListsSingleton getInstance() {
-        return ourInstance;
-    }
+    private static ListsToDoListsSingleton toDoListsSingleton = null;
+
+    private static LinkedList<String> titles;
+    private static LinkedList<String> items;
+    private static LinkedList<String> descriptions;
 
     private ListsToDoListsSingleton() {
+        titles = new LinkedList<String>();
+        items = new LinkedList<String>();
+        descriptions = new LinkedList<String>();
+    }
+
+    public static ListsToDoListsSingleton getInstance() {
+
+        if (toDoListsSingleton == null) {
+            toDoListsSingleton = new ListsToDoListsSingleton();
+            // return ourInstance;
+        }
+        return toDoListsSingleton;
+    }
+
+    public void addName(String title){
+        titles.add(title);
+    }
+
+    public void addItem(String item){
+        items.add(item);
+    }
+
+    public void addDescription(String description){
+        descriptions.add(description);
+    }
+
+    public LinkedList<String>getTitles(){
+        return titles;
+    }
+
+    public LinkedList<String>getItems(){
+        return items;
+    }
+
+    public LinkedList<String>getDescriptions(){
+        return descriptions;
     }
 }
